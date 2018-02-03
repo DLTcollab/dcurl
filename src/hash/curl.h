94ca154f06310620bd117a585317fcc809a7cea4
Chen Wei <c.weiii94@gmail.com>
2018-02-04 00:39:39 +0800

    New Curl interface

---
+++ src/hash/curl.h
@@ -0,0 +1,20 @@
+#ifndef CURL_H_
+#define CURL_H_
+
+#include "../trinary/trinary.h"
+
+#define HASH_LENGTH 243
+#define STATE_LENGTH 3 * HASH_LENGTH
+
+typedef struct _curl {
+    Trits_t *state;
+} Curl;
+
+void Absorb(Curl *c, Trytes_t *inn);
+void Transform(Curl *c);
+Trytes_t *Squeeze(Curl *c);
+
+Curl *initCurl();
+void freeCurl(Curl *c);
+
+#endif
