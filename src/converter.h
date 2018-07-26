98af4d4a9980c1baf7fe258cc38afbfabf6111cd
ajubuntu <ajblane0612@gmail.com>
2018-07-25 19:49:48 -0700

    Rename the functions but do it not well

--- src/converter.h
+++ src/converter.h
@@ -8,12 +8,14 @@ char charValue(char *const trits, const int offset, const int size);
 char *bytes_from_trits(char *const trits, const int offset, const int size);
 void getTrits(const char *bytes, int bytelength, char *const trits, int length);
 int indexOf(char *values, char find);
-char *trits_from_trytes_aj(const char *trytes, int length);
+char *string_trits_from_string_trytes(const char *trytes, int length);
 void copyTrits(char const value,
                char *const destination,
                const int offset,
                const int size);
-char *trytes_from_trits_aj(char *const trits, const int offset, const int size);
+char *string_trytes_from_string_trits(char *const trits,
+                                      const int offset,
+                                      const int size);
 char tryteValue(char *const trits, const int offset);
 
 #endif
