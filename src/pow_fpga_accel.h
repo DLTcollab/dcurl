1086c5a4c09411a9c472979f8b641045503a32f0
ajubuntu <ajblane0612@gmail.com>
2018-08-04 20:30:38 -0700

    Drop unnecessary header files included

--- src/pow_fpga_accel.h
+++ src/pow_fpga_accel.h
@@ -1,12 +1,7 @@
 #ifndef POW_FPGA_ACCEL_H_
 #define POW_FPGA_ACCEL_H_
 
-#include <error.h>
 #include <fcntl.h>
-#include <stdint.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
 #include <sys/mman.h>
 #include <unistd.h>
 #include "trinary.h"
