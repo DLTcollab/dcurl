94ec2c9e47087cb92c8d253bdadcbecd8a654681
zhenwei <zhenwei.tw@gmail.com>
2018-09-02 21:08:41 +0800

    Perform sanity check for OpenCL Library

--- mk/opencl.mk
+++ mk/opencl.mk
@@ -1,7 +1,20 @@
-# FIXME: perform sanity checks before compilation
-OPENCL_LIB ?= /usr/local/cuda/lib64
+# Default the path to the NVIDIA Run time Library
+OPENCL_LIB_PATH ?= /usr/local/cuda/lib64
+
+OPENCL_LIB_DIR := $(shell test -d $(OPENCL_LIB_PATH); echo $$?)
+
+ifneq ($(OPENCL_LIB_DIR),0)
+    $(error "Please specify the path to your OpenCL library on your platform")
+endif
+
+OPENCL_LIB_AVAIL := $(shell ls $(OPENCL_LIB_PATH) | grep -oi libOpenCL.so > /dev/null; echo $$?)
+
+ifneq ($(OPENCL_LIB_AVAIL),0)
+	$(error "Please check the availability of your OpenCL shared library in $(OPENCL_LIB_PATH)")
+endif
 
 CFLAGS += \
         -DENABLE_OPENCL \
-        -I$(OPENCL_PATH)/include
-LDFLAGS += -L$(OPENCL_LIB) -lOpenCL
+        -I$(OPENCL_LIB_PATH)/../include
+
+LDFLAGS += -L$(OPENCL_LIB_PATH) -lOpenCL
