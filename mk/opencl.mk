bda00c3b2fb94c55fa3fa66ba25ede9275b1d6aa
Jim Huang <jserv.tw@gmail.com>
2018-09-17 01:33:37 +0800

    Minor wording tweak

--- mk/opencl.mk
+++ mk/opencl.mk
@@ -1,18 +1,16 @@
 # Default the path to the NVIDIA Run time Library
 OPENCL_LIB_PATH ?= /usr/local/cuda/lib64
-
 OPENCL_LIB_DIR := $(shell test -d $(OPENCL_LIB_PATH); echo $$?)
 
 ifneq ($(OPENCL_LIB_DIR),0)
-    $(error "Please specify the path to your OpenCL library on your platform")
+$(error "Please specify the path to OpenCL library.")
 endif
 
 OPENCL_LIB_AVAIL := $(shell ls $(OPENCL_LIB_PATH) | grep -oi libOpenCL.so > /dev/null; echo $$?)
 
 ifneq ($(OPENCL_LIB_AVAIL),0)
-	$(error "Please check the availability of your OpenCL shared library in $(OPENCL_LIB_PATH)")
+$(error "Please check installation of OpenCL dynamic library in $(OPENCL_LIB_PATH)")
 endif
 
 CFLAGS += -DENABLE_OPENCL
-
 LDFLAGS += -L$(OPENCL_LIB_PATH) -lOpenCL
