4f0c94d72ebd7bc53a24358e01eda67f9810b305
marktwtn <marktwtn@gmail.com>
2019-05-13 18:01:12 +0800

    refactor: Simplification of output debug messages
    Close #153.

--- src/common.h
+++ src/common.h
@@ -1,7 +1,9 @@
 #ifndef COMMON_H_
 #define COMMON_H_
 
+#include <stdarg.h>
 #include <stdint.h>
+#include <stdio.h>
 #include <time.h>
 
 #define __DCURL_MAJOR__ 0
@@ -9,6 +11,15 @@
 #define __DCURL_PATCH__ 0
 
 double diff_in_second(struct timespec t1, struct timespec t2);
+
+static inline void ddprintf(const char *format, ...) {
+#if defined(ENABLE_DEBUG)
+    va_list ap;
+    va_start(ap, format);
+    vprintf(format, ap);
+    va_end(ap);
+#endif
+}
 
 typedef struct _pow_info PoW_Info;
 
