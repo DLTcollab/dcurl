1624c2a7d3e97f84ac2d985099fdd81bc1bbb866
marktwtn <marktwtn@gmail.com>
2019-06-19 12:18:27 +0800

    Release version 0.4.0

--- src/common.h
+++ src/common.h
@@ -7,7 +7,7 @@
 #include <time.h>
 
 #define __DCURL_MAJOR__ 0
-#define __DCURL_MINOR__ 1
+#define __DCURL_MINOR__ 4
 #define __DCURL_PATCH__ 0
 
 double diff_in_second(struct timespec t1, struct timespec t2);
