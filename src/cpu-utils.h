cf0db79f610ca49838ef566b7a3394973bfad194
marktwtn <marktwtn@gmail.com>
2019-09-19 15:11:32 +0800

    feat: Allocate thread resource based on the physical processor
    The original design has contradiction.
    It uses almost all the logical processors for a single PoW,
    but allows dcurl to handle 2 PoW at the same time based on the
    hyper-threading.
    
    The new function is added in cpu-utils.h to detect the thread number per
    physical processor, which also shows whether the hyper-threading is
    enabled or not.
    Once we know the status of hyper-threading, we can determine the
    threadpool size and the thread number for a single PoW.
    
    Close #191.

--- src/cpu-utils.h
+++ src/cpu-utils.h
@@ -15,6 +15,11 @@
 #include <sys/sysinfo.h>
 #endif
 
+/**
+ * @file cpu-utils.h
+ * @brief Utility functions for acquiring CPU information.
+ */
+
 /* On Mac OS X, define our own get_nprocs_conf() */
 #if defined(__APPLE__) || defined(__FreeBSD__)
 #include <sys/sysctl.h>
@@ -28,7 +33,16 @@ static unsigned int get_nprocs_conf()
 }
 #endif
 
-static inline int get_avail_nprocs()
+/**
+ * @brief Get the available number of logical processor.
+ *
+ * Detect the logical processor number with `get_nproc_conf()`
+ * or get the number with the environment variable **DCURL_NUM_CPU**.
+ * @return The available number of logical processor.
+ * If the environment variable **DCURL_NUM_CPU** is not set, return the
+ * **maximum logical processor number - 1**.
+ */
+static inline int get_avail_logic_nprocs()
 {
     size_t nproc = get_nprocs_conf() - 1;
 
@@ -54,4 +68,63 @@ static inline int get_avail_nprocs()
     if (!nproc)
         nproc = 1;
     return nproc;
+}
+
+/**
+ * @brief Get the thread number per physical processor.
+ *
+ * - GNU/Linux: Acquire the thread number by parsing the CPU information.
+ * - macOS: Acquire the thread number by doing the calculation of
+ * (logical processor number / physical processor number).
+ * @return The thread number per physical processor.
+ * @retval 1 Hyperthreading disabled.
+ * @retval 2 Hyperthreading enabled.
+ * @retval -1 Unexpected error.
+ */
+static inline int get_nthds_per_physic_proc()
+{
+    FILE *fd;
+    int nthread;
+#if defined(__linux__)
+    char nthd[4];
+
+    fd = popen("LC_ALL=C lscpu | grep 'Thread(s) per core' | awk '{printf $4}'",
+               "r");
+    if (fd == NULL)
+        return -1;
+    if (fgets(nthd, sizeof(nthd), fd) == NULL)
+        return -1;
+    nthread = (int) strtol(nthd, NULL, 10);
+    if (errno == ERANGE || nthread == 0) {
+        return -1;
+    }
+#elif defined(__APPLE__)
+    char p_proc[4], l_proc[4];
+    int physic_proc, logic_proc;
+
+    fd = popen("sysctl hw.physicalcpu | awk '{printf $2}'", "r");
+    if (fd == NULL)
+        return -1;
+    if (fgets(p_proc, sizeof(p_proc), fd) == NULL)
+        return -1;
+    fd = popen("sysctl hw.logicalcpu | awk '{printf $2}'", "r");
+    if (fd == NULL)
+        return -1;
+    if (fgets(l_proc, sizeof(l_proc), fd) == NULL)
+        return -1;
+    physic_proc = (int) strtol(p_proc, NULL, 10);
+    if (errno == ERANGE || physic_proc == 0) {
+        return -1;
+    }
+    logic_proc = (int) strtol(l_proc, NULL, 10);
+    if (errno == ERANGE || logic_proc == 0) {
+        return -1;
+    }
+
+    nthread = logic_proc / physic_proc;
+#endif
+
+    if (pclose(fd) == -1)
+        return -1;
+    return nthread;
 }
